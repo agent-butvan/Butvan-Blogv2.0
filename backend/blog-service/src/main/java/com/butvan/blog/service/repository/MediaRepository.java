005f4644c6e4bec453f1e4b418cce5ed3d028001
butvan <1973578950@qq.com>
2026-06-20 16:40:17 +0800

    fix(api): 修复MediaRepository未继承JpaSpecificationExecutor导致的编译错误

--- backend/blog-service/src/main/java/com/butvan/blog/service/repository/MediaRepository.java
+++ backend/blog-service/src/main/java/com/butvan/blog/service/repository/MediaRepository.java
@@ -2,11 +2,12 @@ package com.butvan.blog.service.repository;
 
 import com.butvan.blog.pojo.entity.Media;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 /**
  * 媒体资源表 JPA 持久化仓储数据层接口
  */
 @Repository
-public interface MediaRepository extends JpaRepository<Media, Long> {
+public interface MediaRepository extends JpaRepository<Media, Long>, JpaSpecificationExecutor<Media> {
 }
