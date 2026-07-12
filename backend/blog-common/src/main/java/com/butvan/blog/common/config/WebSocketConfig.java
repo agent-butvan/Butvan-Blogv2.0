c33bb0b997454a30d8aac23fd5c58d9edce741f4
butvan <1973578950@qq.com>
2026-07-12 13:11:28 +0800

    fix(config): 恢复 WebSocketConfig 配置类

--- backend/blog-common/src/main/java/com/butvan/blog/common/config/WebSocketConfig.java
+++ backend/blog-common/src/main/java/com/butvan/blog/common/config/WebSocketConfig.java
@@ -1,4 +1,16 @@
 package com.butvan.blog.common.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+@RequiredArgsConstructor
 public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 }
